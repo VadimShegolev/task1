e811d5af608dc9a419ea713bee57dd12518e12ab
Vadim Shegolev <keepman12121998@gmail.com>
2020-10-05 17:44:51 +0600

    Init

---
+++ AlphabeticalText/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlphabeticalText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
